020fcc311eeed036052d488cd7cfca3937455dae
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-03-05 17:17:10 -0800

    population for all teams in history and all their seasons done with scraping

--- scraper.py
+++ scraper.py
@@ -1,7 +1,9 @@
 from soup import CreateSoup
+import json
 
 # Gets the team names and links to those teams
 def get_base_links() -> {}:
+    
     teamMap = {}
     
     # New soup instance
@@ -26,19 +28,47 @@ def get_base_links() -> {}:
 
 # gets the specific HTML page for every team on basketball-reference with an n-season summary
 def get_team_page(teamsMap) -> []:
+    
     pagesForTeams = []
+    
     for name, link in teamsMap.items():
         page = CreateSoup(link)
         pageData = page.process_request()
-        pagesForTeams.append(pageData)
+        pagesForTeams.append({
+            'name': name,
+            'pageData': pageData
+        })
+    
     return pagesForTeams
+
+def process_team_page(teams) -> {}:
+
+    teamSeasons = {}
+
+    # teams is a list of all the teams and their corresponding html @ the link for each teams historical stats
+    for team in teams:
+        
+        teamName = team['name']
+        teamPage = team['pageData']
+
+        # create an array for this team which will hold a list of dictionaries of the season and a link to that specific seasons stats
+        teamSeasons[teamName] = []
+
+        # find the table in the page data and get all the season rows for this team all time
+        allTimeSeasonTable = teamPage.find('tbody').find_all('tr')
+        
+        # for this team, extract the season and the link to the stats for this team in that season
+        for seasons in allTimeSeasonTable:
+            seasonAnchor = seasons.select('th > a')[0]
+            seasonDate = seasonAnchor.text
+            seasonStatsLink = f'https://www.basketball-reference.com{seasonAnchor["href"]}'
+            teamSeasons[teamName].append({'date': seasonDate, 'link': seasonStatsLink})
+
+    return teamSeasons
 
 # main method
 def main() -> None:
-
     teams = get_base_links()
     teamPages = get_team_page(teams)
-
-    print(teamPages[0])
-
+    teamSeasonDictionary = process_team_page(teamPages)
 main()
